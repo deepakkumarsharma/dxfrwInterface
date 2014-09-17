d4fe3b2f970f67d635e0b327149d5d8935a5ae3c
deepakkumarsharma <deeky.sharma@gmail.com>
2014-09-18 01:50:08 +0530

    Point Polyline Arc

--- implementation.cpp
+++ implementation.cpp
@@ -16,7 +16,7 @@ void implementation::addLine(const DRW_Line &data) {
     std::cout << "\n";
 }
 
-void implementation::addCircle(const DRW_Circle& data) {
+void implementation::addCircle(const DRW_Circle &data) {
     std::cout << "#####################\n";
     std::cout << "#       Circle      #\n";
     std::cout << "#####################\n\n";
@@ -29,7 +29,7 @@ void implementation::addCircle(const DRW_Circle& data) {
     std::cout << "\n";
 }
 
-void implementation::addEllipse(const DRW_Ellipse& data) {
+void implementation::addEllipse(const DRW_Ellipse &data) {
     std::cout << "#####################\n";
     std::cout << "#      Ellipse      #\n";
     std::cout << "#####################\n\n";
@@ -51,12 +51,54 @@ void implementation::addEllipse(const DRW_Ellipse& data) {
     std::cout << "\n";
 }
 
-void implementation::addArc(const DRW_Arc& data) {
+void implementation::addArc(const DRW_Arc &data) {
     std::cout << "#####################\n";
     std::cout << "#         Arc       #\n";
     std::cout << "#####################\n\n";
     std::cout << "Start Angle\n"<<data.staangle;
     std::cout << "\n\n";
     std::cout << "End Angle\n"<<data.endangle;
+    std::cout << "\n";
+}
+
+
+void implementation::addPoint(const DRW_Point &data) {
+    std::cout << "#####################\n";
+    std::cout << "#       Point       #\n";
+    std::cout << "#####################\n\n";
+    std::cout << "Base Point \n X\t Y\n";
+    std::cout << data.basePoint.x;
+    std::cout << "\t";
+    std::cout << data.basePoint.y;
+    std::cout << "\n";
+    std::cout << "External Point \n X\t Y\n";
+    std::cout << data.extPoint.x;
+    std::cout << "\t";
+    std::cout << data.extPoint.y;
+    std::cout << "\n";
+}
+
+void implementation::addLWPolyline(const DRW_LWPolyline &data) {
+    std::cout << "#####################\n";
+    std::cout << "#      PolyLine     #\n";
+    std::cout << "#####################\n\n";
+    std::cout << "Vertex\n X\t Y\n";
+    std::cout << data.vertex->x;
+    std::cout << "\t";
+    std::cout << data.vertex->y;
+    std::cout << "\n\n";
+    std::cout << "Start-End Width\n";
+    std::cout << data.vertex->stawidth;
+    std::cout << "\t";
+    std::cout << data.vertex->endwidth;
+    std::cout << "\n\n";
+    std::cout << "Elevation\n";
+    std::cout << data.elevation;
+    std::cout << "\n\n";
+    std::cout << "External Points\n";
+    std::cout << "X \t Y\n";
+    std::cout << data.extPoint.x;
+    std::cout << "\t";
+    std::cout << data.extPoint.y;
     std::cout << "\n";
 }
--- implementation.h
+++ implementation.h
@@ -5,8 +5,10 @@
 
 class implementation : public DRW_InterfaceImpl {
     virtual void addLine(const DRW_Line &data);
-    virtual void addCircle(const DRW_Circle& data);
-    virtual void addEllipse(const DRW_Ellipse& data);
-    virtual void addArc(const DRW_Arc& data);	
+    virtual void addCircle(const DRW_Circle &data);
+    virtual void addEllipse(const DRW_Ellipse &data);
+    virtual void addArc(const DRW_Arc &data);	
+    virtual void addLWPolyline(const DRW_LWPolyline &data);
+    virtual void addPoint(const DRW_Point &data);
 };
 #endif // IMPLEMENTATION_H
